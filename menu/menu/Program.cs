9c3ec3336bb6288c8ae382c51a2111601b642ea1
Viktor7774564 <vitok.joy@gmail.com>
2022-02-04 12:21:45 +0500

    add secOper

--- menu/menu/Program.cs
+++ menu/menu/Program.cs
@@ -27,7 +27,7 @@ namespace menu
                    
                     break;
                 case 2:
-                 
+                    Console.WriteLine(a-b);
                     break;
                 case 3:
                    
