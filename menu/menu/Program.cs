392743ee33d20f24297b08f43cd480e49bff8c6b
VektronD <viktor.dobudko@gmail.com>
2022-02-04 12:42:49 +0500

    add thirOper

--- menu/menu/Program.cs
+++ menu/menu/Program.cs
@@ -30,7 +30,7 @@ namespace menu
                  
                     break;
                 case 3:
-                   
+                    Console.WriteLine(a*b);
                     break;
                 case 4:
                    
